dc125119ab90fc29b22695d28c0b9ec6d51bbc04
Omar Zoghayyer <ozoghayyer11@gmail.com>
2020-08-05 21:37:00 -0700

    Initial Commit

---
+++ oamr/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  oamr
+//
+//  Created by Omar Zoghayyer on 8/5/20.
+//  Copyright © 2020 Omar Zoghayyer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
